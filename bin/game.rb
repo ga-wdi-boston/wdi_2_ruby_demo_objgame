e0253fba29bbe197aaf338313a1e6e107c11bc65
Tom Dyer <tdyer@ourstage.com>
2014-04-28 12:03:18 -0400

    Added Player#take_damage

--- bin/game.rb
+++ bin/game.rb
@@ -2,7 +2,6 @@ require 'pry'
 require_relative '../lib/player'
 puts "Starting the game!!!!"
 
-binding.pry
 jack = Person.new('Jack', 'Sparrow')
 
 puts "Created #{jack.full_name}"
@@ -10,5 +9,9 @@ puts "Created #{jack.full_name}"
 game_man = Player.new("Matt", "Brendzel")
 puts "game man's health is #{game_man.health}"
 
+puts "Game man is " + (game_man.alive? ? "Alive" : "Dead")
+
+game_man.take_damage(30)
+puts "Game man health is #{game_man.health} "
 puts "Game man is " + (game_man.alive? ? "Alive" : "Dead")
 
